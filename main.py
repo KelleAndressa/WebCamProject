da9c4fa5764cfe0650a45c918e8609e357bf51de
Kelle Andressa <101825267+KelleAndressa@users.noreply.github.com>
2022-05-18 20:26:28 +0100

    ...

---
+++ main.py
@@ -0,0 +1,20 @@
+# instalação opencv-python
+
+import cv2
+
+webcam = cv2.VideoCapture(0)
+
+if webcam.isOpened():
+    validacao, frame = webcam.read()
+    while validacao:
+        validacao, frame = webcam.read()
+        cv2.imshow("Video da webcam", frame)
+        key = cv2.waitKey(5)
+        if key == 27:  #ESC
+            break
+
+
+
+
+#Creditos Teacher Lira   Hashtag Programação
+
